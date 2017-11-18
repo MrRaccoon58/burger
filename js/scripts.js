38512ec86ec874b8d7e0c9d2262b467c78e2bda9
Eugene Lukianov (Home PC) <Mrraccoon58@gmail.com>
2017-11-19 00:21:06 +0700

    jshint esversion6

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-$(document).ready(() => {
+/*jshint esversion: 6 */
+$(document).ready(()=>{
     //меню в первой секции
   $(".mobile-menu").hide();
   $(".hamburger").click(e => {
@@ -7,16 +8,16 @@ $(document).ready(() => {
 
     if ($(".hamburger__bars").hasClass('bars-active')) 
     { //вернуть меню
-        $('.mobile-hamburger__bars').fadeOut(300)
-        $(".hamburger__bars").animate({left: '0'}, 300)
-        $(".hamburger__bars").removeClass('bars-active')  
+        $('.mobile-hamburger__bars').fadeOut(300);
+        $(".hamburger__bars").animate({left: '0'}, 300);
+        $(".hamburger__bars").removeClass('bars-active');  
     }
     else 
     { //спрятать меню
-        $('.mobile-hamburger__bars').removeClass('vh')
-        $('.mobile-hamburger__bars').fadeIn(300)
-        $(".hamburger__bars").animate({left: '50px'}, 300)
-        $(".hamburger__bars").addClass('bars-active')
+        $('.mobile-hamburger__bars').removeClass('vh');
+        $('.mobile-hamburger__bars').fadeIn(300);
+        $(".hamburger__bars").animate({left: '50px'}, 300);
+        $(".hamburger__bars").addClass('bars-active');
     }
     
     
