e097c74065b45fb2fab88d8f19e8cdddf4d32dce
Eugene Lukianov (Home PC) <Mrraccoon58@gmail.com>
2017-11-26 17:56:29 +0700

    балуны на карте

--- js/scripts.js
+++ js/scripts.js
@@ -1,123 +1,110 @@
 /*jshint esversion: 6 */
 
-$(function () { //OPS
-  $('.maincontent').onepage_scroll({
+$(function() {
+  //OPS
+  $(".maincontent").onepage_scroll({
     loop: false,
     animationTime: 500
   });
 
   function ops(link, moveto) {
-    $(link).click(function (e) {
+    $(link).click(function(e) {
       e.preventDefault();
-      $('.maincontent').moveTo(moveto);
+      $(".maincontent").moveTo(moveto);
     });
   }
-  ops('.link-2',2);
-  ops('.link-3',3);
-  ops('.link-4',4);
-  ops('.link-5',5);
-  ops('.link-6',6);
-  ops('.link-7',7);
-  ops('.link-8',8);
-
+  ops(".link-2", 2);
+  ops(".link-3", 3);
+  ops(".link-4", 4);
+  ops(".link-5", 5);
+  ops(".link-6", 6);
+  ops(".link-7", 7);
+  ops(".link-8", 8);
 });
 
-
-$(function () { //модалки в ревью
-  $('.review__button').click(function (e) {
+$(function() {
+  //модалки в ревью
+  $(".review__button").click(function(e) {
     e.preventDefault();
     const modalScroll = $("body");
     modalScroll.disablescroll();
     let $this = e.currentTarget;
-    let thisText = $(this).prev('.review__text');
+    let thisText = $(this).prev(".review__text");
     let text = thisText.text();
     swal(text, {
-      button: 'закрыть'
-    }).then((value) => {
+      button: "закрыть"
+    }).then(value => {
       modalScroll.disablescroll("undo");
     });
   });
-
 });
 
 //mailto function
-  $(function () {
+$(function() {
+  let submitForm = function(e) {
+    e.preventDefault();
+    let form = $(e.target);
 
-    let submitForm = function (e) {
-      e.preventDefault();
-      let form = $(e.target);
-          
-      let ajaxForm = function (form) {
-      
-        let url = form.attr('action'),
-            data = form.serialize();
-      
-        return $.ajax({
-            type: 'POST',
-            url: url,
-            data: data,
-            dataType: 'JSON'
+    let ajaxForm = function(form) {
+      let url = form.attr("action"),
+        data = form.serialize();
+
+      return $.ajax({
+        type: "POST",
+        url: url,
+        data: data,
+        dataType: "JSON"
+      });
+    };
+    let request = ajaxForm(form);
+
+    request.done(function(msg) {
+      let mes = msg.mes,
+        status = msg.status;
+      if (status === "OK") {
+        swal(mes, {
+          button: "закрыть"
         });
-      
+      } else {
+        swal(mes, {
+          button: "закрыть"
+        });
       }
-      let request = ajaxForm(form);
-    
-      request.done(function(msg) {
-          let mes = msg.mes,
-              status = msg.status;
-          if (status === 'OK') {
-              swal(mes, {
-                button: 'закрыть'
-              });
-          } else{
-            swal(mes, {
-              button: 'закрыть'
-            });
-          }
+    });
+
+    request.fail(function(jqXHR, textStatus) {
+      swal("Request failed: " + textStatus, {
+        button: "закрыть"
       });
-    
-      request.fail(function(jqXHR, textStatus) {
-        swal("Request failed: " + textStatus, {
-          button: 'закрыть'
-        });
-      });
-    }
-    
-    
-    $('#form__burger-order').on('submit', submitForm);
-  
-  });
+    });
+  };
+
+  $("#form__burger-order").on("submit", submitForm);
+});
 
 //слайдер
-$(window).on('load', function () {
-
-  let owl = $('.owl-carousel');
+$(window).on("load", function() {
+  let owl = $(".owl-carousel");
   owl.owlCarousel({
     items: 1,
     loop: true,
     nav: true
   });
 
-  $('.burgers-list').click(function (e) {
+  $(".burgers-list").click(function(e) {
     e.preventDefault();
-    owl.trigger('next.owl.carousel', [300]);
+    owl.trigger("next.owl.carousel", [300]);
   });
 
-  $('.burgers-controls_prev').click(function (e) {
+  $(".burgers-controls_prev").click(function(e) {
     e.preventDefault();
-    owl.trigger('prev.owl.carousel', [300]);
+    owl.trigger("prev.owl.carousel", [300]);
   });
-
-
-
-
 });
 
-
-
 //аккордеон секции команда
-$(function () {
-  $(".team-acco__trigger").click(function (e) {
+$(function() {
+  $(".team-acco__trigger").click(function(e) {
     e.preventDefault();
     const $this = $(e.currentTarget);
     const container = $this.closest(".team-acco");
@@ -145,7 +132,8 @@ $(function () {
     }
   });
 
-  $(document).mouseup(function (e) { //клик за пределами аккордеона
+  $(document).mouseup(function(e) {
+    //клик за пределами аккордеона
     const container = $(".team-acco__item");
     if (container.has(e.target).length === 0) {
       container.removeClass("team-acco__item_active");
@@ -154,17 +142,19 @@ $(function () {
       });
     }
   });
-
 });
 
 //аккордеон секции меню
-$(function () {
-  $(".menu__acco-item").click(function (e) {
+$(function() {
+  $(".menu__acco-item").click(function(e) {
     const $this = $(e.currentTarget);
     if (!$this.hasClass("menu__acco-item--active")) {
-      $this.siblings().find(".menu__acco-description-wrap").css({
-        opacity: "0"
-      });
+      $this
+        .siblings()
+        .find(".menu__acco-description-wrap")
+        .css({
+          opacity: "0"
+        });
       setTimeout(() => {
         $this.siblings().removeClass("menu__acco-item--active");
         $this.addClass("menu__acco-item--active");
@@ -184,7 +174,8 @@ $(function () {
     }
   });
 
-  $(document).mouseup(function (e) { //клик за пределами аккордеона
+  $(document).mouseup(function(e) {
+    //клик за пределами аккордеона
     const container = $(".menu__acco-item");
     if (container.has(e.target).length === 0) {
       $(".menu__acco-description-wrap").css({
@@ -195,46 +186,44 @@ $(function () {
       }, 250);
     }
   });
-
 });
 
 //меню в первой секции
-$(function () {
+$(function() {
   const menuScroll = $("body");
   $(".hamburger").click(e => {
-
     const $this = e.currentTarget;
     e.preventDefault();
-    if (!$(".mobile-menu").hasClass('mobile-menu_position_active')) {
-      $(".mobile-menu").addClass('mobile-menu_position_active');
-      $(".mobile-menu").addClass('mobile-menu_opacity');
+    if (!$(".mobile-menu").hasClass("mobile-menu_position_active")) {
+      $(".mobile-menu").addClass("mobile-menu_position_active");
+      $(".mobile-menu").addClass("mobile-menu_opacity");
       menuScroll.disablescroll();
-
     } else {
-      $(".mobile-menu").removeClass('mobile-menu_opacity');
+      $(".mobile-menu").removeClass("mobile-menu_opacity");
       setTimeout(() => {
-        $(".mobile-menu").removeClass('mobile-menu_position_active');
+        $(".mobile-menu").removeClass("mobile-menu_position_active");
       }, 300);
       menuScroll.disablescroll("undo");
     }
   });
 
-  $('.mobile-menu__item').click(e => { //при клике отключаем меню
-    $(".mobile-menu").removeClass('mobile-menu_opacity');
+  $(".mobile-menu__item").click(e => {
+    //при клике отключаем меню
+    $(".mobile-menu").removeClass("mobile-menu_opacity");
     setTimeout(() => {
-      $(".mobile-menu").removeClass('mobile-menu_position_active');
+      $(".mobile-menu").removeClass("mobile-menu_position_active");
     }, 300);
     menuScroll.disablescroll("undo");
   });
 
-  $('.mobile-menu__close').click(e => { //закрытие по крестику
-    $(".mobile-menu").removeClass('mobile-menu_opacity');
+  $(".mobile-menu__close").click(e => {
+    //закрытие по крестику
+    $(".mobile-menu").removeClass("mobile-menu_opacity");
     setTimeout(() => {
-      $(".mobile-menu").removeClass('mobile-menu_position_active');
+      $(".mobile-menu").removeClass("mobile-menu_position_active");
     }, 300);
     menuScroll.disablescroll("undo");
   });
-
 });
 
 //yandex карты
@@ -243,26 +232,43 @@ function yamaps() {
 
   function init() {
     var myMap = new ymaps.Map("map", {
-        center: [56.015, 92.87],
-        zoom: 12,
-        controls: []
-      }),
-      burgerCollection = new ymaps.GeoObjectCollection(null, {
-        iconLayout: "default#image",
-        iconImageHref: "./images/8-contacts/marker.png",
-        iconImageSize: [46, 57],
-        iconImageOffset: [0, -40]
-      }),
-      krskBurgersCoords = [
+      center: [56.015, 92.87],
+      zoom: 12,
+      controls: []
+    });
+
+    let cords = [
         [56.01183725, 92.82266731],
         [56.01017297, 92.85667715],
         [55.99333977, 92.89615927],
         [56.03774808, 92.89564428]
+      ],
+      adress = [
+        "улица 1, дом 1",
+        "улица 2, дом 2", 
+        "улица 3, дом 3", 
+        "улица 4, дом 4", 
       ];
-    for (var i = 0, l = krskBurgersCoords.length; i < l; i++) {
-      burgerCollection.add(new ymaps.Placemark(krskBurgersCoords[i]));
+
+    function markcall(cords, adress) {
+      let mark = new ymaps.Placemark(cords, {
+        // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
+        balloonContentHeader: "Mr. Burger",
+        balloonContentBody: adress,
+        balloonContentFooter: "режим работы: 08:00 - 24:00",
+        hintContent: adress}, {
+          iconLayout: 'default#image',
+        iconImageHref: "./images/8-contacts/marker.png",
+        iconImageSize: [46, 57],
+        iconImageOffset: [0, -40]
+      });
+      myMap.geoObjects.add(mark);
     }
-    myMap.geoObjects.add(burgerCollection);
+
+    markcall(cords[0], adress[0]);
+    markcall(cords[1], adress[1]);
+    markcall(cords[2], adress[2]);
+    markcall(cords[3], adress[3]);
     myMap.behaviors.disable("scrollZoom");
   }
 }
